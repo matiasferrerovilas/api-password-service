2db5b8eb8fc59bc29ba9d6fa1f77e7905b743535
Matias <matigfv@gmail.com>
2024-06-29 00:26:44 -0300

    Agregado CI CD

---
+++ src/main/java/api_password_service/passwords/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package api_password_service.passwords.configuration;
+
+import io.swagger.models.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+    configuration.setAllowedHeaders(Collections.singletonList("*"));
+    configuration.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).toList());
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
--- src/main/java/api_password_service/passwords/data/Site.java
+++ src/main/java/api_password_service/passwords/data/Site.java
@@ -1,11 +1,12 @@
 package api_password_service.passwords.data;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @Builder
@@ -13,13 +14,9 @@ import lombok.NoArgsConstructor;
 @Table
 @AllArgsConstructor
 @NoArgsConstructor
-public class Password {
+public class Site {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-  @NotNull
-  private String password;
-  @ManyToOne(optional = false)
-  @JoinColumn(name = "site_id", nullable = false)
-  private Site site;
+  private String descripcion;
 }
